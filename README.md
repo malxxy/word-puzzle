3c242f26380c8e975ab94c1cd72e33d7deca0813
malxxy <maliayraguen@gmail.com>
2022-11-10 21:52:44 -0800

    first commit - set up

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # word-puzzle
 Mini project 4 - UCB
+
+## description
+Guess the word game
+
+Set intervals,
+Timer,
+
+function to set losses
+function to set wins
+
+function to grab losses
+function to grab wins
---
+++ script.js
@@ -0,0 +1,12 @@
+// Pseudocoding
+
+// add event listener
+//  when you click on start 
+
+//  blank array
+
+// Function to set losses
+// Function to set wins
+
+// Function to grab losses
+// Function to grab wins
